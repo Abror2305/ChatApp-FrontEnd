95faa5cfbee82fa688866faf60f8649ecb05d3e0
Abror Alisherov <74832377+Abror2305@users.noreply.github.com>
2022-03-10 19:58:21 +0500

    change some files"

--- login/script.js
+++ login/script.js
@@ -1,4 +1,42 @@
-/* ========================================= * 
-		        BEST VIEWED FULLSCREEN
-   https://codepen.io/ig_design/full/KKVQpVP
- * ========================================= */
+let sSubmit = document.querySelector("#signUp")
+let lSubmit = document.querySelector("#login")
+
+sSubmit.addEventListener("click", function(e){
+
+  let username = document.querySelector("#signUsername")?.value
+  let age = document.querySelector("#signAge")?.value
+  let passwd = document.querySelector("#signPasswd")?.value
+  e.preventDefault()
+
+  console.log(username,age, passwd);
+
+  if(
+    !isValid(username) || 
+    !isValid(passwd) || 
+    isNaN(+age) || 
+    age < 12 || age > 90 ||
+    passwd.length < 6){
+      return alert("WRONG INPUT")
+    }
+
+    console.log("hello")
+    
+    window.localStorage.user_id = 1
+    window.location = "/chat/index.html"
+})
+
+lSubmit.addEventListener("click",(e) => {
+  e.preventDefault()
+  let username = document.querySelector("#logname").value
+  let password = document.querySelector("#logpass").value
+
+  if(!isValid(username) || !isValid(password)|| password.length < 6) return alert("Wrong input")
+
+  window.localStorage.user_id = 1
+  window.location = "/chat/index.html"
+})
+
+function isValid(str){
+  if(!str || str.length < 3  || str.length>20) return false
+  return true
+}
