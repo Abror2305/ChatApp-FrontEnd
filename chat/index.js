4f4f324a5868c125c964ae9ba811860cdd221d0e
Abror Alisherov <74832377+Abror2305@users.noreply.github.com>
2022-03-11 22:28:55 +0500

    register completed

---
+++ chat/index.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded",e => {
+
+	// agar user_id localstorageda bo'lmasa homega qaytarib yuboradi
+	if(!window.localStorage.user_id){
+		window.location = "/"
+	}
+
+	
+})
--- login/script.js
+++ login/script.js
@@ -1,58 +1,59 @@
-let sSubmit = document.querySelector("#signUp")
-let lSubmit = document.querySelector("#login")
-let host = "http://192.168.1.2:4000"
-sSubmit.addEventListener("click", function(e){
+"use strict";
 
-  let username = document.querySelector("#signUsername")?.value
-  let age = document.querySelector("#signAge")?.value
-  let passwd = document.querySelector("#signPasswd")?.value
-  e.preventDefault()
+let sSubmit = document.querySelector("#signUp");
+let lSubmit = document.querySelector("#login");
+let host = "http://192.168.1.3:4000";
+sSubmit.addEventListener("click",async function (e) {
+    let username = document.querySelector("#signUsername")?.value;
+    let age = document.querySelector("#signAge")?.value;
+    let password = document.querySelector("#signPasswd")?.value;
 
-  console.log(username,age, passwd);
+    console.log(username, age, password);
 
-  if(
-    !isValid(username) || 
-    !isValid(passwd) || 
-    isNaN(+age) || 
-    age < 12 || age > 90 ||
-    passwd.length < 6){
-      return alert("WRONG INPUT")
+    const user = {
+        username,
+        age,
+        password
     }
 
-    console.log("hello")
-    
-    window.localStorage.user_id = 1
+    let data = await fetch(host + "/register", {
+        method: "POST",
+        body: JSON.stringify(user),
+    });
+    data = await data.json();
+    console.log(data)
+    if (data?.status !== 200) {
+        return alert(data.message)
+    }
+    window.localStorage.setItem("user_id",data.user_id)
     window.location = "/chat/index.html"
-})
+});
 
-lSubmit.addEventListener("click",async (e) => {
-  e.preventDefault()
-  let username = document.querySelector("#logname").value
-  let password = document.querySelector("#logpass").value
+lSubmit.addEventListener("click", async (e) => {
+    let username = document.querySelector("#logname").value;
+    let password = document.querySelector("#logpass").value;
 
-  console.log(JSON.stringify({username,password}));
-  // if(!isValid(username) || !isValid(password)|| password.length < 6) return alert("Wrong input")
+    console.log(JSON.stringify({ username, password }));
+    if(!isValid(username) || !isValid(password)|| password.length < 6) return alert("Please enter valid username or password")
 
-  const user = {
-    username: username,
-    password: password,
-  }
-  let data = await fetch("http://localhost:4000/login",{
-    method: "POST",
-    headers:{
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json",
-    },
-    mode:"no-cors",
-    body: JSON.stringify( user )
-  })
-  data =await data.json()
-  console.log(data)
-  // console.log(data)
-  // window.location = "/chat/index.html"
-})
+    const user = {
+        username,
+        password,
+    };
 
-function isValid(str){
-  if(!str || str.length < 3  || str.length>20) return false
-  return true
+    let data = await fetch(host + "/login", {
+        method: "POST",
+        body: JSON.stringify(user),
+    });
+    data = await data.json();
+    if (data?.status !== 200) {
+        return alert(data.message)
+    }
+    window.localStorage.setItem("user_id",data.user_id)
+    window.location = "/chat/index.html"
+});
+
+function isValid(str) {
+    if (!str || str.length < 3 || str.length > 20) return false;
+    return true;
 }
